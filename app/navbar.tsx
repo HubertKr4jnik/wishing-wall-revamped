0ac706179f69028746a89c4e77c4fd5f21dd48ac
Hubert Krajnik <h.krajnik714@gmail.com>
2025-07-15 13:08:29 +0200

    added note component and styling

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -15,17 +15,20 @@ export default function Navbar() {
   if (status === "authenticated") {
     console.log(session.user);
     return (
-      <div className="flex flex-row p-4 w-11/12 mx-auto justify-between place-items-center">
+      <div className="flex flex-row p-6 w-11/12 mx-auto justify-between place-items-center">
         <div className="flex flex-row gap-2 text-xl font-bold place-items-center">
           <img
             className="h-11 rounded-full shadow-2xl/100"
             src={session.user?.image}
             alt="Profile Picture"
           />
-          <p>{session.user?.name}</p>
+          <p className="text-shadow-lg">{session.user?.name}</p>
         </div>
-        <button className="flex felx-row rounded px-4 py-2 h-10 text-black text-lg cursor-pointer gap-2 bg-gray-50 hover:scale-105 transition-all">
-          <p onClick={() => signOut()}>Sign Out</p>
+        <button
+          className="flex rounded px-3 py-2 h-10 gap-2 text-black text-lg cursor-pointer bg-slate-200 hover:scale-105 transition-all"
+          onClick={() => signOut()}
+        >
+          <p className="hidden md:block lg:block">Sign Out</p>
           <img
             src="https://cdn-icons-png.flaticon.com/512/7046/7046204.png"
             alt=""
@@ -36,12 +39,15 @@ export default function Navbar() {
   } else {
     return (
       <div className="p-4 w-11/12 mx-auto justify-center place-items-center">
-        <button className="flex felx-row rounded px-4 py-2 h-11 text-black text-xl cursor-pointer gap-2 bg-gray-50 hover:scale-105 transition-all">
+        <button
+          className="flex felx-row rounded px-4 py-2 h-11 text-black text-xl cursor-pointer gap-2 bg-gray-50 hover:scale-105 transition-all"
+          onClick={() => signIn("slack")}
+        >
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Slack_icon_2019.svg"
             alt="Slack Logo"
           />
-          <p onClick={() => signIn("slack")}>Sign In</p>
+          <p>Sign In</p>
         </button>
       </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 import Navbar from "./navbar";
+import Note from "./note";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-[url(https://img.freepik.com/free-photo/dark-brown-wood-texture-background-with-design-space_53876-160410.jpg)] bg-no-repeat bg-cover">
+    <div className="h-screen bg-[url(https://images.pexels.com/photos/172292/pexels-photo-172292.jpeg)] bg-no-repeat bg-cover">
       <Navbar />
-      <div className="m-auto h-10/12 w-11/12 bg-[url(https://img.freepik.com/premium-vector/seamless-background-with-corkboard-texture-corkboard-pinning-notes-todo-lists-photos_314759-1706.jpg)] rounded-xl shadow-2xl/100 border-4 border-stone-700"></div>
+      <div
+        className="m-auto h-10/12 w-11/12 p-6 bg-[url(https://img.freepik.com/premium-vector/seamless-background-with-corkboard-texture-corkboard-pinning-notes-todo-lists-photos_314759-1706.jpg)] rounded-xl shadow-2xl/100 overflow-y-auto [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:rounded-full
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:rounded-full
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+      >
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <div className="flex justify-center place-items-center h-12 w-12 absolute bottom-5 right-5 rounded-xl bg-amber-50 shadow-2xl/100 cursor-pointer hover:scale-110 transition-all">
+          <img
+            className="h-6"
+            src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
